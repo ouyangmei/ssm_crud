352a4b9d88691733773024005cf9c2d680bfa291
ouyangmei <1142378169@qq.com>
2019-03-31 21:51:10 +0800

    day5

--- src/main/Java/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("xxx");
-        System.out.println("YYY");
-        System.out.println("ZZZ");
-
-    }
-}
